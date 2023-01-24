854d9f264cd547b423ba8b04c01e04bbcbd4652f
Long Le <lle@micromine.com>
2023-01-24 19:46:06 +0800

    finalised

--- main.py
+++ main.py
@@ -9,15 +9,6 @@ class World:
         def __str__(self) -> str:
             return f'[{"x" if self.is_filled else " "}]'
 
-        def evolve_val(self, left_box_val: bool, right_box_val: bool):
-            if (
-                (self.is_filled == left_box_val and self.is_filled == right_box_val)
-                or (left_box_val and self.is_filled)
-                or (not self.is_filled and left_box_val and right_box_val)
-            ):
-                return False
-            return True
-
     def __init__(self, size: int = 21) -> None:
         boxes = []
         for i in range(0, size):
@@ -29,17 +20,29 @@ class World:
         self.gen = 0
         self.size = size
 
-    def get_generation(self) -> int:
-        return self.gen
+    @staticmethod
+    def get_new_val(left: bool, middle: bool, right: bool) -> bool:
+        if middle and left:
+            return False
+        elif not middle and left != right:
+            return True
+        return middle
 
     def evolve(self) -> None:
         new_boxes = []
         for i in range(self.size):
             left = False if i == 0 else self.boxes[i - 1].is_filled
+            middle = self.boxes[i].is_filled
             right = False if i == self.size - 1 else self.boxes[i + 1].is_filled
-            new_boxes.append(self.Box(self.boxes[i].evolve_val(left, right)))
+            new_boxes.append(self.Box(World.get_new_val(left, middle, right)))
         self.boxes = new_boxes
         self.gen += 1
+
+    def print_world(self) -> str:
+        result = ""
+        for box in self.boxes:
+            result += "X" if box.is_filled else " "
+        return result
 
     def __str__(self) -> str:
         result = ""
@@ -53,10 +56,10 @@ def main(args):
         the_world = World(args.size)
     else:
         the_world = World()
-    print(f"Start:\t{the_world}")
+    print(f"Start:\t{the_world.print_world()}")
     for i in range(args.gen if args.gen else 10):
         the_world.evolve()
-        print(f"Gen {the_world.gen}:\t{the_world}")
+        print(f"Gen {the_world.gen}:\t{the_world.print_world()}")
 
     pass
 
